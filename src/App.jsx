4e3c307ad4796534d56c0f78113b326ce8a6ec77
Jace <sockgnome@att.net>
2024-05-07 04:54:15 +0000

    Stubbed out required components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import Header from "./components/Header";
+
 function App() {
-  return 'Test'
+  return(
+    <>
+    <Header></Header>
+    <Gallery></Gallery>
+    <Footer></Footer>
+    </>
+  )
 }
 
 export default App;
