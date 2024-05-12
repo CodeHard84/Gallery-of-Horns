9ff6e071d6673cff30d1fb6f1317d3652db1de8d
Jace <sockgnome@att.net>
2024-05-12 02:32:06 +0000

    Feature 1 done.

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Horned Beast
 
 This is just a toy app for React.
+
+## Feature 1: Display Images
+
+Number and name of feature: Display Images
+
+Estimate of time needed to complete: 10 Minutes
+
+Start time: 9:15PM
+
+Finish time: 9:31PM
+
+Actual time needed to complete: 16 Minutes
+
+## Feature Addition Template
+
+Number and name of feature: ________________________________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,11 +1,16 @@
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
 function HornedBeast(props) {
-    return (
+  return (
       <div>
         <h2>{props.title}</h2>
-        <img src={props.imageUrl} alt={props.description} title={props.title} />
+        <Container>
+        <Image src={props.imageUrl} alt={props.description} title={props.title} rounded fluid></Image>
         <p>{props.description}</p>
+        </Container>
       </div>
-    );
-  }
+  );
+}
 
-  export default HornedBeast;
+export default HornedBeast;
