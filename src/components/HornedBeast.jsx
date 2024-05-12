9a736aae0447ef235ccc92a51929fd185cf11bdf
Jace <sockgnome@att.net>
2024-05-12 19:05:12 +0000

    Refactored HornedBeast to use classes.

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,20 +1,29 @@
 import Image from "react-bootstrap/Image";
 import { useState } from "react";
+import React from "react";
 
-export default function HornedBeast(props) {
-
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
+export default class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0
+    };
   }
 
-  return (
-    <span onClick={handleClick}>
-      <h2>{props.title}</h2>
-      <Image src={props.imageUrl} alt={props.description} title={props.title} rounded fluid></Image>
-      <p>{props.description}</p>
-      <h3>Favorites: {"❤️"} = {likes}</h3>
-    </span>
-  );
+  handleClick = () => {
+    this.setState(prevState => ({
+      likes: prevState.likes + 1
+    }));
+  }
+
+  render() {
+    return (
+      <span onClick={this.handleClick}>
+        <h2>{this.props.title}</h2>
+        <Image src={this.props.imageUrl} alt={this.props.description} title={this.props.title} rounded fluid />
+        <p>{this.props.description}</p>
+        <h3>Favorites: {"❤️"} = {this.state.likes}</h3>
+      </span>
+    );
+  }
 }
