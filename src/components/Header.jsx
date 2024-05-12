f8096ba9b70e1d610a89a0dd9e9a3f081eee90a3
Jace <sockgnome@att.net>
2024-05-12 19:01:52 +0000

    Refactored Header to use classes.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
 
-export default function Header(props) {
-    return (
-        <Navbar bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">{props.title}</Navbar.Brand>
-            </Container>
-        </Navbar>
-    );
+export default class Header extends React.Component {
+    render() {
+        return (
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">{this.props.title}</Navbar.Brand>
+                </Container>
+            </Navbar>
+        );
+    }
 }
